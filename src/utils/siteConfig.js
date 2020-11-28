bd16c531d7d3c469e1c2fe71b70bdd568e9650dc
rahul dadsena <71870962+rahuldadz@users.noreply.github.com>
2020-11-28 16:41:39 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://teams.dadsena.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://zealous-yonath-851de3.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
