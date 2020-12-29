458d1e39b7e106291875a5eaf7ec7d71ce11d9d2
rahul dadsena <71870962+rahuldadz@users.noreply.github.com>
2020-12-29 17:09:07 +0530

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,12 +4,12 @@ module.exports = {
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Home`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Advancing Technology, Art & Leadership`, // This allows an alternative site description for meta data for pages.
+    siteDescriptionMeta: `Open and Advanced Learning For All`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Dadsena`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Divyaera`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
